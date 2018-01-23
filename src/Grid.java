1e38b109e5563ab5ce8c9770183a044036f4bd04
MurphyP1 <MurphyP1@104-03-2017.bi.local>
2018-01-23 12:18:06 -0500

    hewwo

--- src/Grid.java
+++ src/Grid.java
@@ -15,13 +15,35 @@ public class Grid {
 		}
 	}
 	
-	
-	
+	//Constructor Helper
 	private void helper() {
 		while(in.hasNextLine()) {
 			
-		}
-			
-			
+		}	
 	}
+	
+	//--------Recursive Optimal Path
+	/* optimalPath(row, col)
+	 * 		if(stack isEmpty) {
+	 * 			base case --> return total num 
+	 * 		if(current point.x = col && currentpoint.y = row) then 
+	 * 			compare the sum of cookies to the optimal pathnum
+	 * 			if(currentSum > highscore) then high = current, 
+	 * 			else 
+	 * 			optimalPath(stack.pop(pathNotTaken) )
+	 * 		If grid(right) and grid[down] are valid, then 
+	 * 			move down
+	 * 			push right to the stack
+	 * 		else if right is valid
+	 * 			move right 
+	 * 		else if down valid 
+	 * 			move down
+	 * 		else no valid moves
+	 * 			optimalPath(stack.pop()) 
+	 * 		}
+	 * 		
+	 * 
+	 */
+	
+	
 }
