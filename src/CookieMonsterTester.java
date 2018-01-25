9c51cafd1c0923f5869ddc23b714a96d2c2c79dc
"[MurphyPOne]" <"[murphyp1@bishopireton.org]">
2018-01-25 17:48:02 -0500

    never makes it to 0,0!!!

--- src/CookieMonsterTester.java
+++ src/CookieMonsterTester.java
@@ -10,5 +10,11 @@ public class CookieMonsterTester {
 		System.out.print(maze);
 		
 		System.out.println("\n The max # of cookies in this file is: " + maze.optimalPath(11, 11) );
+		
+		//Small maze
+		maze = new Grid("cookiesSmall.txt", 4, 4);
+		System.out.print(maze);
+		
+		System.out.println("\n The max # of cookies in this file is: " + maze.optimalPath(3, 3) );
 	}
 }
--- src/Grid.java
+++ src/Grid.java
@@ -1,12 +1,9 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Grid {
 	private int[][] cookies;
-	//private int total;
 	
 	//Constructor
 	public Grid(String fileName, int cols, int rows) {
@@ -32,53 +29,45 @@ public class Grid {
 	
 	//Pathfinding methods
 	public int optimalPath(int col, int row) {
-		//System.out.println(col + ", " + row);
-		if(atOrigin(col, row) ) 
-			return cookies[col][row];	//arrived at origin
-		
-		if(canGoLeft(col, row) && canGoUp(col,row)) { //if both valid
-			int left = optimalPath(col-1, row);	//Save left in var
-			int up = optimalPath(row-1,col);	//save right in var
+		if(!(col == 0 && row == 0) ) {
+			if(canGoUp(col, row) && canGoLeft(col, row)) {	//If two paths
+				int left = optimalPath(col-1, row);	//Run through left
+				int up = optimalPath(col, row-1);	//Run through up
+				
+				if(left > up)	//Compare which had most cookies
+					return cookies[col][row] + left;
+				else 
+					return cookies[col][row] + up;
+			} else if (canGoUp(col, row))	//Can only go up
+				return cookies[col][row] + optimalPath(col, row-1) ;
 			
-			if( left > up)	//see which is better (suuuuper recursive)
-				return cookies[col][row] + left;	//Choose the one that has the greater # cookies
-			else 
-				return cookies[col][row] + up;
-		}
-		if(!canGoLeft(col, row) && canGoUp(col, row)) 	//if top is valid, but Left is not
-			return cookies[col][row] + optimalPath(col, row-1);	//go up
-		
-		if(canGoLeft(col, row) && !canGoUp(col, row)) 	//if left is valid, but Top is not
-			return cookies[col][row] + optimalPath(col-1, row);	//go left
-		 
-		//if reaches end 
-		return cookies[col][row];
-		
-		/* if(!canGoLeft(col, row) && !canGoUp(col, row))	//No valid moves
-		/ 	return total + cookies[col][row];		//return cookies in current cell
-		*/
+			else if (canGoLeft(col, row)) 	//Can only go left
+				return cookies[col][row] + optimalPath(col-1, row);
+			
+			else { 	//Can't go anywhere
+				//System.out.println(col + " " + row); 
+				return cookies[col][row];
+			}
+		} else { System.out.println("Finished!"); return cookies[col][row];  }
 	}	
-	
-	//Directional Helper Methods
-	
+		
 	//Takes in current pos, and checks an adjacent position
 	private boolean canGoLeft(int c, int r) {
-		return ((r > 0 && c > 0) && (cookies[c-1][r]) >= 0 );	//This should work because of short circuiting....
+		return ((r > 0 && c > 0) && (cookies[c-1][r]) >= 0 );	 
 	}
 	
 	private boolean canGoUp(int c, int r) {
 		return ((r > 0 && c > 0) && (cookies[c][r-1]) >= 0 );
 	}
 	
-	private boolean atOrigin(int c, int r) {
-		return (c == 0 && r == 0);
-	}
-	
 	public String toString() {
 		String result = "";
 		for(int i = 0; i < cookies.length; i ++) { 
 			for(int j = 0; j < cookies[0].length; j++) {
-				result += cookies[j][i] + " ";
+				if(cookies[j][i] == -1)
+					result += "X  ";
+				else 
+					result += cookies[j][i] + "  ";
 			}
 			result += "\n";
 		}
