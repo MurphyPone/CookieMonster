151b7dfb3a7d04105b49ec2074da6541c496350d
"[MurphyPOne]" <"[murphyp1@bishopireton.org]">
2018-01-25 23:05:21 -0500

    printPath() not working

--- src/CookieMonsterTester.java
+++ src/CookieMonsterTester.java
@@ -1,20 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.Stack;
-
 public class CookieMonsterTester {
 
 	public static void main(String[] args) {
+		//Supplied maze 
 		Grid maze = new Grid("cookies.txt", 12, 12);
 		System.out.print(maze);
-		
 		System.out.println("\n The max # of cookies in this file is: " + maze.optimalPath(11, 11) );
 		
 		//Small maze
 		maze = new Grid("cookiesSmall.txt", 4, 4);
 		System.out.print(maze);
-		
 		System.out.println("\n The max # of cookies in this file is: " + maze.optimalPath(3, 3) );
+		
+		//random maze 
+		int size = 3;
+		maze = new Grid(size);
+		System.out.print(maze);
+		System.out.println("\n The max # of cookies in this file is: " + maze.optimalPath(size-1, size-1) );
+		System.out.print(maze.printCoords());
 	}
 }
--- src/Grid.java
+++ src/Grid.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Grid {
-	private int[][] cookies;
-	
-	//Constructor
+	private int[][] cookies;	//Contents of the grid
+	private Stack<Integer> pathX;	//Stores the monsters x coords
+	private Stack<Integer> pathY;	//Stores the monsters Y coords
+
+	//Constructor with supplied file
 	public Grid(String fileName, int cols, int rows) {
-		cookies = new int[cols][rows];
-		//total = 0;
+		cookies = new int[cols][rows];	//Initialize structures
+		pathX = new Stack<Integer>();
+		pathY = new Stack<Integer>();
+
 		Scanner inputFile = null;
 
 		try	{
@@ -27,42 +32,65 @@ public class Grid {
 		inputFile.close();
 	}	
 	
+	//Constructor for random mazes of a set size 
+	public Grid(int size) {
+		cookies = new int[size][size];
+		pathX = new Stack<Integer>();
+		pathY = new Stack<Integer>();
+		
+		for (int r = 0; r < size; r++) {
+			for (int c = 0; c < size; c++) {
+				cookies[c][r] = (int) (Math.random() * 10) - 1;	//-1 to add the occasional barrel
+			}
+		}
+	}	
+	
 	//Pathfinding methods
 	public int optimalPath(int col, int row) {
-		if(!(col == 0 && row == 0) ) {
-			if(canGoUp(col, row) && canGoLeft(col, row)) {	//If two paths
-				int left = optimalPath(col-1, row);	//Run through left
-				int up = optimalPath(col, row-1);	//Run through up
-				
-				if(left > up)	//Compare which had most cookies
-					return cookies[col][row] + left;
-				else 
-					return cookies[col][row] + up;
-			} else if (canGoUp(col, row))	//Can only go up
-				return cookies[col][row] + optimalPath(col, row-1) ;
+		if(cookies[col][row] == -1) 	//Invalid maze
+			return -9999;
+		
+		if(canGoUp(col, row) && canGoLeft(col, row)) {	//If two paths
+			int left = optimalPath(col-1, row);	//Run through left
+			int up = optimalPath(col, row-1);	//Run through up
 			
-			else if (canGoLeft(col, row)) 	//Can only go left
-				return cookies[col][row] + optimalPath(col-1, row);
-			
-			else { 	//Can't go anywhere
-				//System.out.println(col + " " + row); 
-				return cookies[col][row];
+			if(left > up) {	//Compare which had most cookies
+				savePath(col, row);
+				return cookies[col][row] + left;
+			} else {  
+				savePath(col, row);
+				return cookies[col][row] + up;
 			}
-		} else { System.out.println("Finished!"); return cookies[col][row];  }
+		} else if (canGoUp(col, row)) {	//Can only go up
+			savePath(col, row);
+			return cookies[col][row] + optimalPath(col, row-1) ;
+		} else if (canGoLeft(col, row)) { 	//Can only go left
+			savePath(col, row);
+			return cookies[col][row] + optimalPath(col-1, row);
+		} else { 	//Can't go anywhere
+			savePath(col, row);
+			return cookies[col][row];
+		}
 	}	
 		
 	//Takes in current pos, and checks an adjacent position
 	private boolean canGoLeft(int c, int r) {
-		return ((r > 0 && c > 0) && (cookies[c-1][r]) >= 0 );	 
+		return ((r >= 0 && c > 0) && (cookies[c-1][r]) >= 0 );	 
 	}
 	
 	private boolean canGoUp(int c, int r) {
-		return ((r > 0 && c > 0) && (cookies[c][r-1]) >= 0 );
+		return ((r > 0 && c >= 0) && (cookies[c][r-1]) >= 0 );
+	}
+	
+	//Stack helper method
+	private void savePath(int col, int row) {
+		pathX.push(col);
+		pathY.push(row);
 	}
 	
 	public String toString() {
 		String result = "";
-		for(int i = 0; i < cookies.length; i ++) { 
+		for(int i = 0; i < cookies.length; i++) { 
 			for(int j = 0; j < cookies[0].length; j++) {
 				if(cookies[j][i] == -1)
 					result += "X  ";
@@ -74,5 +102,30 @@ public class Grid {
 		
 		return result;
 	}
+	
+	public String printCoords() {
+		String result = "\nFormatted for X,Y coordinate pairs";
+		while(!pathX.isEmpty())
+			result += Integer.toString(pathX.pop()+1) + ", " + Integer.toString(pathY.pop()+1) + "\n";
+		return result;
+	}
+	
+	public String printPath() {
+		String result = "";
+		for(int i = 0; i < cookies.length; i++) { 	//for each r
+			for(int j = 0; j < cookies[0].length; j++) {	//for each c
+			
+				if(i == pathY.peek() && j == pathX.peek()) { //If current point in traversal equals the next set of points in the stack
+					result += "P  ";
+					pathY.pop();
+					pathX.pop();
+				} else 
+					result += "X  ";
+			}
+			result += "\n";
+		}
+		return result;
+	}
+	
 }
 
